8750db4bf6e5f660703f7049f59a064c63fd5bb5
Vjacheslav Kanivetc <vjacheslav.kanivetc@gmail.com>
2018-12-11 19:21:38 +0200

    support for automatic batch execution to all supported languages

---
+++ GoogleTranslate/translate_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage $0 /path/to/src/main/res"
+  exit
+fi
+
+if [ ! -d $1 ]; then
+  echo "Usage $0 /path/to/src/main/res (path not found!)"
+  exit
+fi
+
+logfile=output.log
+echo . > $logfile
+for f in $1/values-b*; do
+  lang=${f: -2}
+  newfile=strings_$lang.xml
+  echo "translating to $lang -> $newfile"
+  python3 gtranslate.py en $lang input.xml $newfile >> $logfile 2>&1
+done
+
+echo "all done! you can check logs in $logfile"
